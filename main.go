238d769e39775e7f4460e657b9ddf79b772944d4
Stephan Chandler-Garcia <stephan@gdprsuperheroes.com>
2020-05-11 14:58:51 -0700

    update to error handling

--- main.go
+++ main.go
@@ -29,36 +29,41 @@ func main() {
 }
 
 func getSchools(w http.ResponseWriter, r *http.Request) {
-	body, err := MakeRequest("https://code.org/schools.json")
-	if err != nil {
-		fmt.Println(err)
-	}
 
+	// Get the latitude URL paramater
 	latString, ok := r.URL.Query()["lat"]
 	if !ok || len(latString[0]) < 1 {
 		log.Println("Url Param 'lat' is missing")
-		fmt.Println("Url Param 'lat' is missing")
 		http.Error(w, "Bad Request: Url Param 'lat' is missing", 400)
 		json.NewEncoder(w).Encode(http.Error)
 		return
 	}
 
+	// Get the longitude URL paramater
 	lonString, ok := r.URL.Query()["lon"]
 	if !ok || len(lonString[0]) < 1 {
-		http.Error(w, "Bad Request: Url Param 'lat' is missing", 400)
+		http.Error(w, "Bad Request: Url Param 'lon' is missing", 400)
 		json.NewEncoder(w).Encode(http.Error)
 		return
 	}
 
+	// Convert the lat & lon to strings
 	latFloat, err := strconv.ParseFloat(latString[0], 64)
 	lonFloat, err := strconv.ParseFloat(lonString[0], 64)
-
-	var schools School
-
-	json.Unmarshal(body, &schools)
-
 	currentLocation := haversine.Coord{Lat: latFloat, Lon: lonFloat}
 
+	// Make Request to the code.org Schools API
+	body, err := MakeRequest("https://code.org/schools.json")
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		fmt.Println(err)
+	}
+
+	// parse the JSON response body
+	var schools School
+	json.Unmarshal(body, &schools)
+
+	// Loop through the schools and calculate the distance from the supplied cordinates
 	for i := 0; i < len(schools.Schools); i++ {
 		schoolLocation := haversine.Coord{Lat: schools.Schools[i].Latitude, Lon: schools.Schools[i].Longitude}
 		mi, km := haversine.Distance(currentLocation, schoolLocation)
@@ -66,20 +71,25 @@ func getSchools(w http.ResponseWriter, r *http.Request) {
 		schools.Schools[i].DistanceKM = km
 	}
 
+	// Sort the schools by their distance from the supplied cordinates
 	sort.Slice(schools.Schools, func(i, j int) bool { return schools.Schools[i].Distance < schools.Schools[j].Distance })
 
+	// Loop through the schools and get the nearest three for the response
+	// todo: find a way to reuse the same struct instead of creating a duplicate
 	var finalSchools []IsolatedSchools
-
 	for i := 0; i < 3; i++ {
 		finalSchools = append(finalSchools, schools.Schools[i])
 		fmt.Println("User Type: " + schools.Schools[i].Name)
 		fmt.Printf("%f Miles\n", schools.Schools[i].Distance)
 	}
+
+	// Take the list of nearby schools & serialize the response
 	finalSchoolsAsString, err := json.Marshal(finalSchools)
 	if err != nil {
 		panic(err)
 	}
-	w.Header().Add("Content-Type", "application/json")
+
+	// Log and return the result
 	fmt.Println(string(finalSchoolsAsString))
 	json.NewEncoder(w).Encode(finalSchools)
 
@@ -146,12 +156,6 @@ type IsolatedSchools struct {
 	Source            string      `json:"source"`
 	Distance          float64
 	DistanceKM        float64
-}
-
-type appError struct {
-	Error   error
-	Message string
-	Code    int
 }
 
 func MakeRequest(url string) ([]byte, error) {
